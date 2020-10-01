f2ca74bf1f44e892fbe075015336709ba0557260
Vivek Sanghvi <viksipart@gmail.com>
2020-10-01 18:06:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,11 @@
+#def reverse_each_word(sent) 
+
+  #sent.split.map {|word| word.reverse}.join(" ")
+
+  
+  
+#end
+
 def reverse_each_word(sent) 
 
   sent.split.collect {|word| word.reverse}.join(" ")
